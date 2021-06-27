969fc94d1901e4c396886b3aa482ab55ed32d7f7
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2021-06-27 17:01:59 +0530

    Time: 104 ms (86.62%), Space: 43.4 MB (73.00%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,31 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    debugger;
-    var resObj = {
-        str: '',
-        max: 0
-    };
-    for (let c of s) {
-        if (!resObj[c]) {
-            updateString(c, resObj);
+ var lengthOfLongestSubstring = function(s) {
+    //going to use the sliding window pattern here
+    let left = 0;
+    let right = 0;
+    let max = 0;
+    let strSet = new Set();
+    // right will only slide when a valid condition is met
+    while(right < s.length) {
+        let c = s[right];
+        if(!strSet.has(c)) {
+            strSet.add(c);
+            max = Math.max(max, strSet.size);
+            right++;
         } else {
-            while (resObj[c]) {
-                let presentStr = resObj.str[0];
-                resObj.str = resObj.str.slice(1);
-                resObj[presentStr] = false;
-            }
-            updateString(c, resObj);
+            strSet.delete(s[left]);
+            left++;
         }
     }
-    return resObj.max;
-};
-
-function updateString(c, resObj) {
-    resObj[c] = true;
-    resObj.str += c;
-    if (resObj.max < resObj.str.length) {
-        resObj.max = resObj.str.length;
-    }
+    return max;
 }
