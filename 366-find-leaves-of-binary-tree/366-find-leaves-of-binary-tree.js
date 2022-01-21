1e5a73c20aed017b94008a6df6151bb8d9813868
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-01-21 19:03:22 +0530

    Time: 138 ms (5.42%), Space: 39.6 MB (81.18%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
@@ -11,6 +11,7 @@
  * @return {number[][]}
  */
 var findLeaves = function(root) {
+    // traverse to remove all the leaf nodes 
     function traverse(node,res) {
         if(node == null ) return null;
         if(node.left == null && node.right == null) {
@@ -22,25 +23,13 @@ var findLeaves = function(root) {
         return node;
     }
     let res = [];
-    while(root.left !=null || root.right!=null) {
+    //loop while we only have root node left
+    while(root!=null) {
         let arr = [];
-        traverse(root,arr);
+        root = traverse(root,arr);
         res.push(arr);
     }
-    res.push([root.val]);
     return res;
 };
 
-function Node(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-}
-var btree = function(arr,i=0) {    
-    if(i>arr.length) return null;
-    let root = new Node(arr[i]);
-    root.left = btree(arr,2*i+1);
-    root.right = btree(arr,2*i+2);
-    return root;
-    
-}
+
