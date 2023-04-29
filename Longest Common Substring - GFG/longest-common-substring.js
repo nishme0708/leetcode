cb163e8f44d29f4d30ef78381a70c9c82789be8e
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-04-29 19:12:10 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.js
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size) {
+  let i;
+  let s = "";
+  for (i = 0; i < size; i++) {
+    s += arr[i] + " ";
+  }
+  console.log(s);
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let input1 = readLine().split(" ").map((x) => parseInt(x));
+    let n = input1[0];
+    let m = input1[1];
+    let S1 = readLine();
+    let S2 = readLine();
+    let obj = new Solution();
+    let res = obj.longestCommonSubstr(S1,S2,n,m);
+    console.log(res);
+  }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {string} S1
+ * @param {string} S2
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+
+class Solution {
+    longestCommonSubstr(S1,S2,n,m){
+        //code here
+        let table = Array(n+1).fill().map(()=>Array(m+1).fill(0));
+        let max = 0;
+        for(let i=0;i<=n;i++) {
+            for(let j=0;j<=m;j++) {
+                if(i ==0 || j ==0) {
+                    table[i][j] = 0;
+                    
+                } else {
+                    if(S1[i-1] == S2[j-1]) {
+                        table[i][j] = table[i-1][j-1] +1;
+                        max = Math.max(max, table[i][j]);
+                    } else {
+                        table[i][j] = 0;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
