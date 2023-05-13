9a4557880f200b8bc6fc7a96c2918cc01c1a8bec
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-13 19:17:34 +0530

    Time: 79 ms, Memory: 46.2 MB - LeetHub

---
+++ jump-game/jump-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let reach = 0;
+    for(let i=0;i<=reach;i++) {
+        reach = Math.max(reach, nums[i]+i);
+        if(reach>=nums.length-1){
+            return true;
+        }
+    }
+    return false;
+};
