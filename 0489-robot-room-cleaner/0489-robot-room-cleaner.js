569a130c8b211b97bde70833283877c45fac458e
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-14 13:55:14 +0530

    Time: 83 ms (93.62%), Space: 46.4 MB (87.23%) - LeetHub

--- 0489-robot-room-cleaner/0489-robot-room-cleaner.js
+++ 0489-robot-room-cleaner/0489-robot-room-cleaner.js
@@ -38,7 +38,7 @@
 var cleanRoom = function (robot) {
   let visited = new Set();
    
-  
+ // Checking if the row and col are within limit and the room is cleanable 
 function checkValid(room, row, col) {
   if (row < 0 || row >= room.length || col < 0 || col >= room[row].length) {
     return false;
@@ -48,48 +48,67 @@ function checkValid(room, row, col) {
 }
 
 function traverse(row,col,robot) {  
+ // Marking row,col as visited so we don't visit this again
   visited.add(row+','+col);
+  // Cleaning it
   robot.clean();
+  
+  // Checking if top is valid
  let top = checkValid(robot.room, row - 1, col);
   if (top) {
     robot.move();
     traverse(row - 1, col,robot);
+	
+	//Backtracking
     robot.turnRight();
     robot.turnRight();
     robot.move();
     robot.turnRight();
     robot.turnRight();
   }
+  
+  // Checking if right move is valid
   let right = checkValid(robot.room, row, col + 1);
   if (right) {
     robot.turnRight();
     robot.move();
+	
+	// Moving to default orientation of top
     robot.turnLeft();
     traverse(row, col + 1,robot);
+	
+	//Back tracking
     robot.turnLeft();
     robot.move();
     robot.turnRight();
   }
 
+// checking if left move is valid
   let left = checkValid(robot.room, row, col - 1);
   if (left) {
     robot.turnLeft();
     robot.move();
+	// Moving to default orientation of top
     robot.turnRight();
     traverse(row, col - 1,robot);
+	
+	// Backtracking
     robot.turnRight();
     robot.move();
     robot.turnLeft();
   }
 
+// Checking if down move is valid
   let down = checkValid(robot.room, row + 1, col);
   if (down) {
     robot.turnRight();
     robot.turnRight();
     robot.move();
+	// Moving to default orientation of top
     robot.turnRight();
     robot.turnRight();
     traverse(row + 1, col,robot);
+	// Backtracking
     robot.move();
   }
 }  
