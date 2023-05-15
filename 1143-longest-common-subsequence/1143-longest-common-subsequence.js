11d2bc4e4ecbdd7ddbce6f811eaf400238b16304
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-15 15:13:12 +0530

    Time: 135 ms (58.47%), Space: 73.2 MB (27.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -3,41 +3,20 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequenceRec = function(  a,
-  b,
-  m = a.length - 1,
-  n = b.length - 1,
-  memo = {}) {
-      let key = m + '-' + n;
-  if (memo[key]) return memo[key];
-  if(m<0 || n<0) return 0;
-  let one = a[m];
-  let two = b[n];
-  if (one == two) {
-    memo[key] = 1 + longestCommonSubsequence(a, b, m - 1, n - 1, memo);
-  } else {
-    memo[key] = Math.max(
-      longestCommonSubsequence(a, b, m - 1, n, memo),
-      longestCommonSubsequence(a, b, m, n - 1, memo)
-    );
-  }
-  return memo[key];
-};
-
-
-var longestCommonSubsequence = function(  a,  b) {
-    let m = a.length;
-    let n = b.length;
+var longestCommonSubsequence = function(text1, text2) {
+    let m = text1.length;
+    let n = text2.length;
+    if(m== 0 || n ==0 ) return 0;
     let table = Array(m+1).fill().map(()=>Array(n+1).fill(0));
-    for(let i = 0;i<=m;i++) {
-        for(let j =0;j<=n;j++) {
-            if(i == 0 || j ==0) {
-                table[i][j] = 0;
+    for(let i=0;i<=m;i++) {
+        for(let j=0;j<=n;j++) {
+            if(i==0 || j==0){
+                table[i][j]=0;
             } else {
-                if(a[i-1] == b[j-1]) {
-                    table[i][j] = 1+ table[i-1][j-1];
+                if(text1[i-1] == text2[j-1]) {
+                    table[i][j] = 1 + table[i-1][j-1];
                 } else {
-                    table[i][j] = Math.max(table[i-1][j],table[i][j-1]);
+                    table[i][j] = Math.max(table[i][j-1],table[i-1][j]);
                 }
             }
         }
