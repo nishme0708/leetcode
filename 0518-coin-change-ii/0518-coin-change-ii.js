6571a666d75aff302aa9af2d261e575d8b96f7f1
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-15 14:05:53 +0530

    Time: 128 ms (35.11%), Space: 70.3 MB (19.43%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -6,35 +6,21 @@
 var change = function(amount, coins) {
     let n = coins.length;
     let table = Array(n+1).fill().map(()=>Array(amount+1).fill(0));
-    for(let i = 0;i<=n;i++) {
-        for(let j=0;j<=amount;j++) {
-            if(j == 0) {
+    for(let i=0;i<=n;i++) {
+        for(let j=0;j<=amount;j++) {            
+            if(j==0) {
                 table[i][j]=1;
+            } else if(i==0) {
+                table[i][j] = 0;
             } else {
-                if(i == 0) {
-                    table[i][j] = 0;
+                let rem = j-coins[i-1];
+                if(rem>=0) {
+                    table[i][j] = table[i-1][j] + table[i][rem];
                 } else {
-                    let rem = j - coins[i-1];
-                    let total = table[i-1][j];
-                    if(rem >= 0) {
-                        total+=table[i][rem];
-                    }
-                    table[i][j] = total;
+                    table[i][j] = table[i-1][j];
                 }
             }
         }
-    }
-    
+    }    
     return table[n][amount];
 };
-
-var recur = function( amount, coins, n=0,memo={}) {
-    if(amount < 0) return 0;
-    if(amount == 0 ) return 1;
-    if(n>= coins.length) return 0;
-    let key = amount + '-' + n;
-    if(memo[key]) return memo[key];
-    let item = coins[n];
-    memo[key] = change(amount - item, coins, n, memo) + change(amount, coins, n+1,memo);
-    return memo[key];
-}
