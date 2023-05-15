b5ebf5642efcdedc926880843bc85d4acdabb391
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-15 14:40:25 +0530

    Time: 168 ms, Memory: 55.8 MB - LeetHub

---
+++ coin-change/coin-change.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let n = coins.length;
+    let table = Array(n+1).fill().map(()=>Array(amount+1).fill(Number.MAX_VALUE));
+    for(let i=0;i<=n;i++) {
+        for(let j=0;j<=amount;j++) {
+            if(j==0) {
+                table[i][j] = 0;
+            } else if(i==0){
+                continue;
+            } else {
+                let rem = j-coins[i-1];
+                if(rem>=0){
+                    table[i][j]=Math.min(1+table[i][rem],table[i-1][j]);
+                }else{
+                    table[i][j]=table[i-1][j];
+                }
+            }
+        }
+    }
+    return table[n][amount] == Number.MAX_VALUE ? -1 : table[n][amount] ;
+};
