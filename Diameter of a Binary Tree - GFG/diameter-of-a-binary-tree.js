4f9c8f844f152d189d7ebd0777c39d37ed2c0158
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-05-29 13:07:22 +0530

    Added solution - LeetHub

---
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.js
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str) {
+    // Corner Case
+    if (str.length === 0 || str[0] === "N") return null;
+
+    // Create the root of the tree
+    let root = new Node(parseInt(str[0]));
+
+    // Push the root to the queue
+    let queue = [];
+    let start = 0;
+    queue.push(root);
+
+    // Starting from the second element
+    let i = 1;
+    while (queue.length !== start && i < str.length) {
+
+        // Get and remove the front of the queue
+        let currNode = queue[start];
+        start++;
+
+        // Get the current node's value from the string
+        let currVal = str[i];
+
+        // If the left child is not null
+        if (currVal !== "N") {
+
+            // Create the left child for the current node
+            currNode.left = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= str.length) break;
+        currVal = str[i];
+
+        // If the right child is not null
+        if (currVal !== "N") {
+
+            // Create the right child for the current node
+            currNode.right = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let input_ar1 = readLine().split(' ');
+        let root = buildTree(input_ar1);
+        let obj = new Solution();
+        console.log(obj.diameter(root));
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {Node} root
+ * @returns {number}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to return the diameter of a Binary Tree.
+    diameter(root) {
+        // your code here
+        if(!root) return 0;
+        if(root.left == null && root.right == null) return 1;
+        let max = 0;
+        function traverse(node) {
+            if(!node) return 0;
+            if(node.left == null && node.right == null) return 1;
+            let left = traverse(node.left);
+            let right = traverse(node.right);
+            let path = 1 + Math.max(left, right);
+            max = Math.max(max, 1+left+right);
+            return path;
+        }
+        
+        return Math.max(traverse(root),max);
+    }
+}
