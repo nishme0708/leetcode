44e6667fecb7930958b6df7959bb85b828b11a33
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2023-07-05 10:55:25 +0530

    Time: 115 ms (37.74%), Space: 69.6 MB (33.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -3,24 +3,26 @@
  * @param {number[]} coins
  * @return {number}
  */
-var change = function(amount, coins) {
+var change = function (amount, coins) {
     let n = coins.length;
-    let table = Array(n+1).fill().map(()=>Array(amount+1).fill(0));
-    for(let i=0;i<=n;i++) {
-        for(let j=0;j<=amount;j++) {            
-            if(j==0) {
-                table[i][j]=1;
-            } else if(i==0) {
-                table[i][j] = 0;
+    let table = Array(n + 1).fill().map(() => Array(amount+1).fill(0));
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j <= amount; j++) {
+            if (j == 0) {
+                table[i][j] = 1;
+            } else if (i == 0) {
+                continue;
             } else {
-                let rem = j-coins[i-1];
-                if(rem>=0) {
-                    table[i][j] = table[i-1][j] + table[i][rem];
+                let curr = coins[i - 1];
+                let rem = j - curr;
+                if (rem >= 0) {
+                    table[i][j] = table[i][rem] + table[i - 1][j];
                 } else {
-                    table[i][j] = table[i-1][j];
+                    table[i][j] = table[i - 1][j];
                 }
             }
         }
-    }    
-    return table[n][amount];
+    }
+    
+    return table[n][amount]
 };
